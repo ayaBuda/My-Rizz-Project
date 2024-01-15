7808cfacec1edbbca1c36126a11773810ce8599c
unknown <ayandabuda73@gmail.com>
2024-01-15 13:09:33 +0200

    adding image links

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const noButton = document.querySelector(".noButton");
 yesButton.addEventListener("click", () => {
   message.innerHTML = "Yay, see you on the 17th!";
   gif.src =
-    "https://www.bing.com/images/search?view=detailV2&ccid=97nBsPqf&id=5A8795AAEA124F29654E0E2BC028322943474490&thid=OIP.97nBsPqfU5pY1wKwa5-tBwHaFa&mediaurl=https%3a%2f%2fmedia1.tenor.com%2fimages%2ff7b9c1b0fa9f539a58d702b06b9fad07%2ftenor.gif%3fitemid%3d15238868&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.f7b9c1b0fa9f539a58d702b06b9fad07%3frik%3dkERHQykyKMArDg%26pid%3dImgRaw%26r%3d0&exph=364&expw=498&q=tom+and+jerry+love+gif&simid=608033220715033177&FORM=IRPRST&ck=E3AF4E802D0F781F62A48B31645CAFD8&selectedIndex=0&itb=0&idpp=overlayview&ajaxhist=0&ajaxserp=0";
+    "https://giphy.com/gifs/boomerangtoons-tom-and-jerry-cat-5dUllWbKVlaqmMTvHb";
 });
 
 noButton.addEventListener("mouseover", () => {
